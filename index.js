c54564b0ef677093bae89442bef0b20c530e993f
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2022-09-24 16:53:23 -0400

    commit tree 09335a17bd563e268e07a668a08cb022d058f9b5

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(saturdayActivity = "roller-skate"){
+    return(`This Saturday, I want to ${saturdayActivity}!`)
+}
+saturdayFun()
+saturdayFun("sleep")
+console.log(saturdayFun())
+console.log(saturdayFun("sleep"))
+
+const mondayWork = function (mondayActivity = "go to the office") {
+    return(`This Monday, I will ${mondayActivity}.`)
+}
+mondayWork()
+mondayWork("dance salsa")
+console.log(mondayWork())
+console.log(mondayWork("dance salsa"))
+
+// function wrapAdjective(adjective = "special") {
+//   return function () {(`You are *${adjective}*!`)
+// }
+// }
+const wrapAdjective = function(highlight = "*") {
+    return function(adjective = "special") { 
+        return `You are ${highlight}${adjective}${highlight}!`
+    }
+}
